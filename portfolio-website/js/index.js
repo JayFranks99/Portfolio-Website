f4617dcc8c66cda0bad20ce3338ba986b115abba
Jay Franks <59868381+JayFranks99@users.noreply.github.com>
2020-06-27 16:33:19 +0100

    Portfolio filter using buttons added

--- portfolio-website/js/index.js
+++ portfolio-website/js/index.js
@@ -15,16 +15,52 @@ $(document).ready(function(){
     $('.sub-menu-items .active').toggleClass('.sub-menu-items .active');
   });
 
-//SMOOTH ANIMATION ON CLICK
+//SMOOTH ANIMATION ON CLICK - DOWN
   $(".down-arrow").click(function() {
     $('html, body').animate({
         scrollTop: $("#about").offset().top
     }, 1000);
 });
 
+//SMOOTH ANIMATION ON CLICK - UP
+
 $(".up-arrow").click(function() {
   $('html, body').animate({
       scrollTop: $("header").offset().top
   }, 1500);
 });
+
+
+//DEVELOPMENT ON
+$('.development-switch').click(function () {
+  $('#development').attr('class','display-block');
+  $('#design').attr('class','display-none');
+  $('#editing').attr('class','display-none');
+  $('#animation').attr('class','display-none');
+});
+
+//DESIGN ON
+$('.design-switch').click(function () {
+  $('#design').attr('class','display-block');
+  $('#development').attr('class','display-none');
+  $('#editing').attr('class','display-none');
+  $('#animation').attr('class','display-none');
+});
+
+//EDITING ON
+$('.editing-switch').click(function () {
+  $('#editing').attr('class','display-block');
+  $('#development').attr('class','display-none');
+  $('#design').attr('class','display-none');
+  $('#animation').attr('class','display-none');
+});
+
+//ANIMATION ON
+$('.animation-switch').click(function () {
+  $('#animation').attr('class','display-block');
+  $('#development').attr('class','display-none');
+  $('#editing').attr('class','display-none');
+  $('#design').attr('class','display-none');
+});
+
 })
