c0a0fd2df5a138bebb1779c8d1100ff829196925
Jay Franks <59868381+JayFranks99@users.noreply.github.com>
2020-07-06 14:36:16 +0100

    Home and Portfolio
    RESPONSIVENESS AND PAGES COMPLETED

--- js/index.js
+++ js/index.js
@@ -97,11 +97,9 @@ var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
 img.onclick = function(){
-  modal.style.display = "block";
+  modal.style.display = "flex";
   modalImg.src = "images/altitude-lg-img.png"
-  captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
@@ -120,11 +118,10 @@ var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg2");
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+
 img.onclick = function(){
-  modal.style.display = "block";
+  modal.style.display = "flex";
   modalImg.src = "images/SLGC-lg-img.png";
-  captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
@@ -145,13 +142,11 @@ var modalClass = document.querySelector(".modal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg3");
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+
 img.onclick = function(){
-  modal.style.display = "block";
-  $('.modal').addClass('paddingTop');
+  modal.style.display = "flex";
   $('.modal-content').addClass('heightImage');
   modalImg.src = "images/jazz-lg-img.png";
-  captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
@@ -160,7 +155,6 @@ var span = document.getElementsByClassName("close-x")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-  $('.modal').removeClass('paddingTop');
   $('.modal-content').removeClass('heightImage');
 }
 
