988e5871d53b013bb41bc53bf8294834607d4de0
Jay Franks <59868381+JayFranks99@users.noreply.github.com>
2020-07-06 14:50:11 +0100

    False alarm - fully responsive

--- js/index.js
+++ js/index.js
@@ -97,7 +97,7 @@ var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
-img.onclick = function(){
+img.onclick = function() {
   modal.style.display = "flex";
   modalImg.src = "images/altitude-lg-img.png"
 }
